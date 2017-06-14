2066049ecd8a79f962b170cf1e4e579fc496d484
skmehra <skumar.mnit@gmail.com>
2017-06-14 21:06:07 +0530

    Create app.py

--- app.py
+++ app.py
@@ -86,8 +86,8 @@ def makeWebhookResult(data):
     if condition is None:
         return {}
     
-    print("json.dumps: ")
-    print(json.dumps(item, indent=4))
+    print("json.condition: ")
+    print(json.dumps(condition, indent=4))
 
     speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
              ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
